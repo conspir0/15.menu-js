73dbdcf43ab3e354ae6327eb6b0709be252b17ee
M Michalczyk <pan.michalczyk@gmail.com>
2018-08-25 02:13:25 +0200

    Create HTML DOM structure in JS

--- js/script.js
+++ js/script.js
@@ -2,11 +2,40 @@
 
 document.addEventListener('DOMContentLoaded', function() {
     
-    console.log('Hello World!');
+    // Create ul
+    const createUl = document.createElement('ul');
+    createUl.classList.add('nav');
     
-    const title = document.querySelector('h1');
+    document.body.appendChild(createUl);
     
-    title.addEventListener('click', function() {
-        title.classList.toggle('change-font-color');
-    })
+    let createLi = '';
+    let createLink = '';
+    for(let i = 0; i < 6; i++) {
+        
+    // Create ul > li
+    createLi = document.createElement('li');
+    createLi.classList.add('nav-elem');
+    createUl.appendChild(createLi);
+    
+    // Create ul > li > a
+    createLink = document.createElement('a');
+    createLink.classList.add('nav-link');
+    createLink.href = '#';
+    createLi.appendChild(createLink);
+    }
+    // Add active class to first li element
+    createUl.firstChild.classList.add('nav-elem-active');
+    
+    // Create constances
+    const ul = document.querySelector('.nav');
+    const li = ul.querySelectorAll('.nav-elem');
+    
+    // Add names of menu boxes
+    li[0].children[0].innerText = 'Home';
+    li[1].children[0].innerText = 'Offer';
+    li[2].children[0].innerText = 'Gallery';
+    li[3].children[0].innerText = 'About Us';
+    li[4].children[0].innerText = 'Our Team';
+    li[5].children[0].innerText = 'Contact';
+
 })
